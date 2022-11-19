9aceab2ad25f50123bd7c2f9bfeb0ab0934eb49b
Ali Malek <ali.malek@snapp-box.com>
2022-11-19 18:15:02 +0330

    feat(exception): handle exceptions by using exception controller advice

---
+++ src/main/java/ir/alimalek/assignment/exception/ExceptionDTO.java
@@ -0,0 +1,13 @@
+package ir.alimalek.assignment.exception;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ExceptionDTO {
+    private String message;
+}
---
+++ src/main/java/ir/alimalek/assignment/exception/HttpException.java
@@ -0,0 +1,13 @@
+package ir.alimalek.assignment.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public class HttpException extends RuntimeException {
+    private final HttpStatus statusCode;
+    private final String errorMessage;
+
+}
